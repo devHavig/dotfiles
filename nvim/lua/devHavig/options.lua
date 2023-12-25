e9e21c08c496825ecab21484bdb403342606804f
devhavig <devhavig@gmail.com>
2023-12-24 21:56:29 -0800

    moved options and keymaps to seperate files

---
+++ nvim/lua/devHavig/options.lua
@@ -0,0 +1,45 @@
+local options = {
+    guicursor = "",
+    mouse = 'a',
+    tabstop = 1,
+    shiftwidth = 4,
+    softtabstop = 4,
+    expandtab = true,
+    smartindent = true,
+    wrap = false,
+    scrolloff = 8,
+    updatetime = 250,
+    timeoutlen = 300,
+    clipboard = 'unnamedplus'
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable break indent 
+vim.o.breakindent = true
+
+-- Save undo history 
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS /C or capital in search
+vim.oignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default 
+vim.wo.signcolumn = 'yes'
+
+-- Set completeopt to have a better completion experience 
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure you terminal supports this
+vim.o.termguicolors = true
+
+
