bc94b3f8da45aaf8c615137886fd1e092c56de6b
devhavig <devhavig@gmail.com>
2024-01-01 18:07:20 -0800

    changed status bar theme

--- nvim/lua/devHavig/lazy.lua
+++ nvim/lua/devHavig/lazy.lua
@@ -153,10 +153,11 @@ require("lazy").setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'horizon',
         component_separators = '|',
         section_separators = '',
       },
