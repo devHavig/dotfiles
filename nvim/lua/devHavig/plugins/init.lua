df0b5107b26180b9cdc25f2a1fbaeec0fd65261d
devhavig <devhavig@gmail.com>
2024-01-08 14:39:29 -0800

    adds gofmt and gopls

--- nvim/lua/devHavig/plugins/init.lua
+++ nvim/lua/devHavig/plugins/init.lua
@@ -45,6 +45,7 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.gofmt,
           null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.black,
           null_ls.builtins.diagnostics.eslint_d,
