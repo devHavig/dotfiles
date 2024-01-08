e46325623e7acc2758ea6e7c9e0cf5420b3d3757
devhavig <devhavig@gmail.com>
2024-01-08 14:54:13 -0800

    adds auto-format on save

--- nvim/lua/devHavig/plugins/none-ls.lua
+++ nvim/lua/devHavig/plugins/none-ls.lua
@@ -1,18 +1,33 @@
 return {
-    -- Formatter 
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
+  -- Formatter
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.gofmt,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.diagnostics.eslint_d,
-        },
-      })
-    end,
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.eslint_d,
+      },
+      -- Auto-Format on Save
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({
+            group = augroup,
+            buffer = bufnr,
+          })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
+  end,
 }
-
